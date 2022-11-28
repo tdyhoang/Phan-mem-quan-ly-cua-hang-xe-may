413f0c4ad8354bb97bf34326085a4676bd3cf236
tdyhoang <tdyhoang@gmail.com>
2022-11-28 14:33:07 +0700

    Minor fixes to locating

--- Demo/obj/Debug/Demo_Content.g.i.cs
+++ Demo/obj/Debug/Demo_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("lock.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/lock.png")]
 
 
