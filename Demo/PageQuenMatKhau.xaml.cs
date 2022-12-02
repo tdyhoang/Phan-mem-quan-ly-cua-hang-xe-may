b2974ce50747e5c398b533f2d3b9a478f99897f7
cauruax1503 <106080372+Turtl3x1503@users.noreply.github.com>
2022-12-03 01:49:16 +0700

    Update Login
    Cập Nhật Login Form Lần 4 qua Branch dự phòng, đã tick hết các file mình sửa, các file không liên quan thì không tick.

---
+++ Demo/PageQuenMatKhau.xaml.cs
@@ -0,0 +1,170 @@
+﻿using Demo.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net.Mail;
+using System.Net;
+using System.Security.RightsManagement;
+
+namespace Demo
+{
+    /// <summary>
+    /// Interaction logic for PageQuenMatKhau.xaml
+    /// </summary>
+    public partial class PageQuenMatKhau : Page
+    {
+        static private PageChinh pgC; //Tạo biến kiểu PageChinh để có thể sử dụng trong class này
+        public PageQuenMatKhau(PageChinh pageChinh)
+        {
+            InitializeComponent();
+            pgC = pageChinh; //Gán biến pgC chính là tham số pageChinh được cõng theo
+            //Hàm Khởi Tạo của trang QuenMatKhau có cõng theo tham số là pageChinh thuộc kiểu PageChinh
+        }
+        static public long ma;  //Đặt biến tĩnh để các PageGuiMa có thể truy cập*/
+        public PageGuiMa pgGM = new PageGuiMa(pgC);
+        static public string strEmail;
+        static public string ngonngu = "Tiếng Việt";
+        private int flag = 0;
+
+        private void buttonLanguageQMK_Click(object sender, RoutedEventArgs e)
+        {
+            switch (buttonLanguage.Content)
+            {
+                case "Tiếng Việt":
+                    lblNnguHienTai.Content = "Current Language:";
+                    buttonLanguage.Content = "English";
+                    lblEmail.Content = "Your Email:";
+                    lbldoiPass.Content = "New Password:";
+                    lblXacNhandoiPass.FontSize = 18;
+                    lblXacNhandoiPass.Content = "Retype New Password:";
+                    buttonXacNhan.Content = "Confirm";
+                    buttonQuayLai.Content = "Back";
+                    if (flag == 1)
+                        lblThongBao.Content = "Please fill all of your fields fully!";
+                    pgGM.lblThongBao.Content = "We had sent a code with 6 numbers to your Email, please fill it below:";
+                    pgGM.buttonXacNhanGuiMa.Content = "Verify";
+                    pgC.lblNnguHienTai.Content = "Current Language:";
+                    pgC.buttonLanguage.Content = "English";
+                    pgC.txtTenTK.Text = "Username:";
+                    pgC.txtMatKhau.Text = "Password:";
+                    pgC.buttonDangNhap.Content = "Login";
+                    pgC.buttonQuenMK.FontSize = 14;
+                    pgC.buttonQuenMK.Content = "Forgot Password ?";
+                    pgC.txtQLYCHXM.FontSize = 22;
+                    pgC.txtQLYCHXM.Text = "MOTORCYCLE SHOP MANAGER";
+                    pgC.txtSlogan.Text = "We bring the best solution for manager";
+                    break;
+                case "English":
+                    lblNnguHienTai.Content = "Ngôn Ngữ Hiện Tại:";
+                    buttonLanguage.Content = "Tiếng Việt";
+                    lblEmail.Content = "Nhập Địa Chỉ Email:";
+                    lbldoiPass.Content = "Nhập Mật Khẩu Mới:";
+                    lblXacNhandoiPass.FontSize = 18;
+                    lblXacNhandoiPass.Content = "Xác Nhận Mật Khẩu:";
+                    buttonXacNhan.Content = "Xác Nhận";
+                    buttonQuayLai.Content = "Quay Lại";
+                    if (flag == 1)
+                        lblThongBao.Content = "Vui lòng điền đầy đủ thông tin của bạn!";
+                    pgGM.lblThongBao.Content = "Chúng tôi đã gửi mã 6 số về địa chỉ Email bạn cung cấp, xin hãy điền nó xuống dưới:";
+                    pgGM.buttonXacNhanGuiMa.Content = "Xác Nhận";
+                    pgC.buttonLanguage.Content = "Tiếng Việt";
+                    pgC.txtTenTK.Text = "Tên Tài Khoản:";
+                    pgC.txtMatKhau.Text = "Mật Khẩu:";
+                    pgC.buttonDangNhap.Content = "Đăng Nhập";
+                    pgC.buttonQuenMK.Content = "Quên Mật Khẩu ?";
+                    pgC.txtQLYCHXM.Text = "QUẢN LÝ CỬA HÀNG XE MÁY";
+                    pgC.txtSlogan.Text = " Chúng tôi mang đến giải pháp tốt nhất cho nhà quản lý";
+                    pgC.lblNnguHienTai.Content = "Ngôn Ngữ Hiện Tại:";
+                    break;
+            }
+            ngonngu = buttonLanguage.Content.ToString();
+        } 
+
+        private void buttonXacNhan_Click(object sender, RoutedEventArgs e)
+        {
+            flag = 1;  //Báo hiệu nút Xác Nhận đã được click
+            if (string.IsNullOrEmpty(txtEmail.Text) == true || string.IsNullOrEmpty(txtDoiPass.Password)==true||string.IsNullOrEmpty(txtXacNhanDoiPass.Password)==true)
+            {
+                if (buttonLanguage.Content == "English")
+                    lblThongBao.Content = "Please Fill All Fields Fully!";
+                else
+                    lblThongBao.Content = "Vui Lòng Điền Đầy Đủ Thông Tin!";
+            }
+            else if(txtDoiPass.Password!=txtXacNhanDoiPass.Password)
+            {
+                if (buttonLanguage.Content == "English")
+                    lblThongBao.Content = "Password Retype Didn't Match New Password, Check Again!";
+                else
+                    lblThongBao.Content = "Mật Khẩu Xác Nhận Không Khớp Với Mật Khẩu Mới, Kiểm Tra Lại!";
+                txtXacNhanDoiPass.Clear();
+                txtXacNhanDoiPass.Focus();
+            }
+            else
+            {
+                GuiMail();
+                var pageGuiMa = new PageGuiMa(pgC);
+                switch(buttonLanguage.Content)
+                {
+                    case "English":
+                        pageGuiMa.lblThongBao.Content = "We had sent a code with 6 numbers to your Email, fill it below:";
+                        pageGuiMa.buttonXacNhanGuiMa.Content = "Verify";
+                        break;
+                    case "Tiếng Việt":
+                        pageGuiMa.lblThongBao.Content = "Chúng tôi đã gửi mã 6 số về Email của bạn, điền nó xuống dưới:";
+                        pageGuiMa.buttonXacNhanGuiMa.Content = "Xác Nhận";
+                        break;
+                }
+                NavigationService.Navigate(pageGuiMa);
+                //Nếu thoả mãn hết các điều kiện kiểm tra thì chuyển hướng tiếp sang Trang GuiMa
+            }
+        }
+
+        private void GuiMail()
+        {
+            MailMessage mess = new MailMessage();
+            mess.From = new MailAddress("datrua3152003@gmail.com"); //Đây là Email gửi từ ứng dụng
+            Random rand = new Random();
+            ma = rand.Next(100000, 999999);
+            switch (buttonLanguage.Content)
+            {
+                case "Tiếng Việt":
+                    mess.Subject = "Mã Xác Nhận Thay Đổi Mật Khẩu";
+                    mess.Body = "Mã Xác Nhận Của Bạn Là: " + ma.ToString();
+                    break;
+                case "English":
+                    mess.Subject = "Verify Change Password Code";
+                    mess.Body = "Your Verify Code Is: " + ma.ToString();
+                    break;
+            }
+            mess.To.Add(new MailAddress(txtEmail.Text));   //Email nhận là của người Nhân Viên Quản Lý
+            SmtpClient smtpClient1 = new SmtpClient("smtp.gmail.com")
+            {
+                Port = 587,
+                Credentials = new NetworkCredential("datrua3152003@gmail.com", "ewxeolqofypyfzgi"),
+                EnableSsl = true,
+            };
+            smtpClient1.Send(mess);
+
+            /* Hàm này để ứng dụng gửi mã 6 số ngẫu nhiên
+              về Email người Quản Lý để xác nhận thay đổi mật khẩu */
+        }
+
+        private void buttonQuayLai_Click(object sender, RoutedEventArgs e)
+        { 
+            this.NavigationService.Navigate(pgC);
+
+            //Hàm này để Quay lại Trang Chính(pgC)
+        }
+    }
+}
--- Demo/View/LoginView.xaml.cs
+++ Demo/View/LoginView.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -11,7 +12,9 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 /*Trong Quá Trình Chạy Login Form, Nếu Gặp Lỗi 
  Unable to copy file "obj\Debug\Demo.exe" to "bin\Debug\Demo.exe".
  The process cannot access the file 'bin\Debug\Demo.exe' because it is being used by another process.Demo:
@@ -28,13 +31,11 @@ namespace Demo.View
     /// </summary>
     public partial class LoginView : Window
     {
-        QuenMatKhau qmk = new QuenMatKhau();
-        static public GuiMa gm = new GuiMa();
-        static public string ngonngu; //Đặt biến tĩnh để các form sau có thể truy cập
         public LoginView()
         {
             InitializeComponent();
         }
+
         private void Window_MouseDown(object sender, MouseButtonEventArgs e)
         {
             if (e.LeftButton == MouseButtonState.Pressed)
@@ -45,78 +46,11 @@ namespace Demo.View
         {
             WindowState = WindowState.Minimized;
         }
+
         private void btnClose_Click(object sender, RoutedEventArgs e)
         {
             Application.Current.Shutdown();
         }
 
-        private void buttonLanguage_Click(object sender, RoutedEventArgs e)
-        {
-            switch (buttonLanguage.Content)
-            {
-                case "Tiếng Việt":
-                    buttonLanguage.Content = "English";
-                    txtTenTK.Text = "Username:";
-                    txtMatKhau.Text = "Password:";
-                    buttonDangNhap.Content = "Login";
-                    buttonQuenMK.Content = "Forgot Password ?";
-                    txtQLYCHXM.FontSize = 23;
-                    txtQLYCHXM.Text = "MOTORCYCLE SHOP MANAGER";
-                    lblNnguHienTai.Content = "Current Language:";
-                    qmk.lblEmail.Content = "Your Email:";
-                    qmk.lbldoiPass.Content = "New Password:";
-                    qmk.lblXacNhandoiPass.FontSize = 18;
-                    qmk.lblXacNhandoiPass.Content = "Retype New Password:";
-                    qmk.buttonXacNhan.Content = "Confirm";
-                    qmk.Title = "Reset Password";
-                    gm.Title = "Verify Code";
-                    gm.lblGuiMa.Content = "We had sent a code with 6 numbers to your Email you provided, fill that code below:";
-                    gm.buttonXacNhanMa.Content = "Verify";
-                    ngonngu = buttonLanguage.Content.ToString(); //Cập nhật biến ngôn ngữ
-                    break;
-                case "English":
-                    buttonLanguage.Content = "Tiếng Việt";
-                    txtTenTK.Text = "Tên Tài Khoản:";
-                    txtMatKhau.Text = "Mật Khẩu:";
-                    buttonDangNhap.Content = "Đăng Nhập";
-                    buttonQuenMK.Content = "Quên Mật Khẩu ?";
-                    txtQLYCHXM.Text = "QUẢN LÝ CỬA HÀNG XE MÁY";
-                    lblNnguHienTai.Content = "Ngôn Ngữ Hiện Tại:";
-                    qmk.lblEmail.Content = "Nhập Địa Chỉ Email:";
-                    qmk.lbldoiPass.Content = "Nhập Mật Khẩu Mới:";
-                    qmk.lblXacNhandoiPass.Content = "Xác Nhận Mật Khẩu:";
-                    qmk.buttonXacNhan.Content = "Xác Nhận";
-                    qmk.Title = "Thiết Lập Lại Mật Khẩu";
-                    gm.Title = "Nhập Mã";
-                    gm.lblGuiMa.Content = "Chúng tôi đã gửi mã 6 số về Email mà bạn cung cấp, hãy điền mã đó xuống dưới:";
-                    gm.buttonXacNhanMa.Content = "Xác Nhận";
-                    ngonngu = buttonLanguage.Content.ToString(); //Cập nhật biến ngôn ngữ
-                    break;
-            }
-        }
-
-        private void buttonDangNhap_Click(object sender, RoutedEventArgs e)
-        {
-            
-            if(string.IsNullOrEmpty(txtUser.Text)==true||string.IsNullOrEmpty(txtPassword.Password))
-            {     if (buttonLanguage.Content=="English")
-                {
-                    MessageBox.Show("Please fill in all fields completely and correctly!");
-                }
-                else
-                {
-                    MessageBox.Show("Vui lòng điền đầy đủ thông tin!");
-                }
-            }
-            else
-            {
-
-            }
-        }
-
-        private void buttonQuenMK_Click(object sender, RoutedEventArgs e)
-        {
-            qmk.Show();  
-        }
     }
 }
