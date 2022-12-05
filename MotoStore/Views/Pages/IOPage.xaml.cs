11a10404ba124a94871c3c9c1aa23a5de1d9aa9d
DungLe2983 <lequocdung2983@gmail.com>
2022-12-05 21:06:58 +0700

    IO_Designs
    Thêm tính năng Nhập cho các thuộc tính như: Sản Phẩm, Khách hàng, NSX,..

--- MotoStore/Views/Pages/IOPage.xaml.cs
+++ MotoStore/Views/Pages/IOPage.xaml.cs
@@ -18,5 +18,25 @@ namespace MotoStore.Views.Pages
 
             InitializeComponent();
         }
+
+        private void btnAddSPPage_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            IOMain.Content=new IOPagePages.IOSanPhamPage();
+        }
+
+        private void btnAddNVPage_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            IOMain.Content= new IOPagePages.IONhanVienPage();
+        }
+
+        private void btnAddNSXPage_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            IOMain.Content=new IOPagePages.IONhaSXPage();
+        }
+
+        private void btnAddKhachHang_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            IOMain.Content = new IOPagePages.IOKhachHangPage();
+        }
     }
 }
