d63b9932633262120eddc9056f1cefd0c806ec65
tdyhoang <tdyhoang@gmail.com>
2022-12-08 15:09:13 +0700

    Database hotfix

--- src/MotoStore/Database/QlychbanxemayContext.cs
+++ src/MotoStore/Database/QlychbanxemayContext.cs
@@ -4,13 +4,13 @@ using Microsoft.EntityFrameworkCore;
 
 namespace MotoStore.Database;
 
-public partial class MainDatabase : DbContext
+public partial class QlychbanxemayContext : DbContext
 {
-    public MainDatabase()
+    public QlychbanxemayContext()
     {
     }
 
-    public MainDatabase(DbContextOptions<MainDatabase> options)
+    public QlychbanxemayContext(DbContextOptions<QlychbanxemayContext> options)
         : base(options)
     {
     }
@@ -32,7 +32,6 @@ public partial class MainDatabase : DbContext
     public virtual DbSet<UserAdmin> UserAdmins { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
         => optionsBuilder.UseSqlServer("Server=.\\SQLExpress;Database=QLYCHBANXEMAY;Encrypt=False;Trusted_Connection=True;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
