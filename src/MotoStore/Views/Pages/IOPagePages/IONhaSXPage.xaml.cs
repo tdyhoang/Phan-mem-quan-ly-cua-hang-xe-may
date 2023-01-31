ff35f89ab618bc315da2873c5231840c1d907b0e
DungLe2983 <lequocdung2983@gmail.com>
2023-01-31 13:13:25 +0700

    NCCPage(Done)
    Thay thế các lbl Thong báo bằng ValidationMode
    Add Parameter

--- src/MotoStore/Views/Pages/IOPagePages/IONhaSXPage.xaml.cs
+++ src/MotoStore/Views/Pages/IOPagePages/IONhaSXPage.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
@@ -13,55 +15,95 @@ namespace MotoStore.Views.Pages.IOPagePages
     /// </summary>
     public partial class IONhaSXPage : Page
     {
-        bool checkTenNCC = false;
+        internal List<Control> InputFields;
+       
         public IONhaSXPage()
         {
+
             InitializeComponent();
+            PageInitialization();   
         }
+        private void PageInitialization()
+        {
+            InputFields = new()
+            {
+                txtTenNCC,
+                txtSDTNSX,
+                txtEmailNSX,
+                txtDiaChi
+            };
+        }
+
 
         private void btnAddNewNSX_Click(object sender, RoutedEventArgs e)
-        {           
-            SqlConnection con = new(Properties.Settings.Default.ConnectionString);
-            SqlCommand cmd;
-            if (!checkTenNCC)
-                MessageBox.Show("Các Trường Dữ Liệu Quan Trọng(Có Dấu (*) Đang Bị Thiếu), Vui Lòng Kiểm Tra Lại");
-            else
+        {
+            foreach (var c in InputFields.Where(c => Validation.GetHasError(c)))
             {
-                con.Open();
-                cmd = new("Set Dateformat dmy\nInsert into NhaCungCap values('" + txtTenNCC.Text + "','" + txtSDTNSX.Text + "','" + txtEmailNSX.Text + "',N'" + txtDiaChi.Text + " ' ,0)", con);
-                cmd.ExecuteNonQuery();
-                cmd = new SqlCommand("Select top(1) MaNCC from NhaCungCap order by ID desc", con);
-                SqlDataReader sda = cmd.ExecuteReader();
-                string NCCMoi = "NCC@";
-                if (sda.Read())
-                    NCCMoi = (string)sda[0];
-                DateTime now = DateTime.Now;
-                cmd = new($"Set Dateformat dmy\nInsert into LichSuHoatDong values(NEWID(), '{PageChinh.getNV.MaNv}', '{now:dd-MM-yyyy HH:mm:ss}', N'thêm mới Nhà Cung Cấp " + NCCMoi + "')", con);
-                cmd.ExecuteNonQuery();
-                con.Close();
-                MessageBox.Show("Thêm dữ liệu thành công");
+                MessageBox.Show("Dữ liệu đang có lỗi, không thể thêm!");
+                return;
+            }         
+            if(string.IsNullOrEmpty(txtTenNCC.Text) || string.IsNullOrEmpty(txtEmailNSX.Text))
+            {
+                MessageBox.Show("Các ô được đánh dấu * không được để trống!");
+                return;
+            }    
+          
+            using SqlConnection con = new(Properties.Settings.Default.ConnectionString);
+            SqlCommand cmd;         
+            {
+                try
+                {
+                    con.Open();
+                    using var trans = con.BeginTransaction();
+                    try
+                    {
+                        cmd = new("Set Dateformat dmy\nInsert into NhaCungCap values(@TenNCC,@SDT,@Email,@DiaChi,0)", con, trans);
+                        cmd.Parameters.Add("@TenNCC", System.Data.SqlDbType.NVarChar);
+                        cmd.Parameters["@TenNCC"].Value = txtTenNCC.Text;
+                        cmd.Parameters.Add("@SDT", System.Data.SqlDbType.VarChar);
+                        cmd.Parameters["@SDT"].Value = txtSDTNSX.Text;
+                        cmd.Parameters.Add("@Email", System.Data.SqlDbType.NVarChar);
+                        cmd.Parameters["@Email"].Value = txtEmailNSX.Text;
+                        cmd.Parameters.Add("@DiaChi", System.Data.SqlDbType.NVarChar);
+                        cmd.Parameters["@DiaChi"].Value = txtDiaChi.Text;
+                        cmd.ExecuteNonQuery();
+
+                        cmd = new SqlCommand("Select top(1) MaNCC from NhaCungCap order by ID desc", con, trans);
+                        SqlDataReader sda = cmd.ExecuteReader();
+                        string NCCMoi = "NCC@";
+                        if (sda.Read())
+                        {
+                            NCCMoi = (string)sda[0];
+                            sda.Close();    
+                        }
+                        DateTime now = DateTime.Now;
+                        cmd = new($"Set Dateformat dmy\nInsert into LichSuHoatDong values(NEWID(), '{PageChinh.getNV.MaNv}', '{now:dd-MM-yyyy HH:mm:ss}', N'thêm mới Nhà Cung Cấp " + NCCMoi + "')", con, trans);
+                        cmd.ExecuteNonQuery();
+                        trans.Commit();
+
+                        
+                        MessageBox.Show("Thêm dữ liệu thành công");
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Thêm mới thất bại, Lỗi: " + ex.Message);
+                        trans.Rollback();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
             }
         }
-
-        //check Email
-        private void txtEmailNSX_LostFocus(object sender, RoutedEventArgs e)
+        private void InputField_LostFocus(object sender, RoutedEventArgs e)
         {
-            if (!txtEmailNSX.Text.Contains("@gmail.com"))
-                lblThongBaoEmail.Visibility = Visibility.Visible;
-            else
-                lblThongBaoEmail.Visibility = Visibility.Collapsed;
+            if (sender is TextBox tb)
+                tb.GetBindingExpression(TextBox.TextProperty).UpdateSource();
+           
         }
 
-        private void txtTenNCC_LostFocus(object sender, RoutedEventArgs e)
-        {
-            if (string.IsNullOrWhiteSpace(txtTenNCC.Text))
-                lblThongBao.Visibility = Visibility.Visible;
-            else
-            {
-                checkTenNCC = true;
-                lblThongBao.Visibility = Visibility.Collapsed;
-            }
-        }
+        
     }
 }
 
